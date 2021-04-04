4d35614213cf29402ce85f1dc8f8dff1311d1903
BeatLink <beatlink+git@simplelogin.co>
2021-04-04 16:51:05 -0500

    Update Recurrence Dialog

--- src/index.ts
+++ src/index.ts
@@ -25,26 +25,92 @@ joplin.plugins.register({
 
 		const handle3 = await dialogs.create('myDialog3');
 		await dialogs.setHtml(handle3, `
-		<p>Task Recurrence</p>
+		<h1>Repeating To-Do</h1>
 		<form name="recurrence">
-
-			<section>
-				<label for="recurrenceEnabled">Repeat Enabled: </label>
+			<fieldset> 
+				<legend>Enabled</legend>
 				<input type="checkbox" id="recurrenceEnabled" name="recurrenceEnabled" value="True">
-			</section>
-			
-			<section>
-				<label for="repeatPeriod">Repeat Period: </label>
+				<label for="recurrenceEnabled">This Task Repeats</label></td>
+			</fieldset>
+			</br>
+			<fieldset>
+				<legend>Interval</legend>
 				<input type="number" id="repeatPeriod" name="repeatPeriod" min="1" max="999" step="1" value="1">
 				<select name="repeatInterval">
 					<option value="minute" selected>Minute</option>
 					<option value="hour">Hour</option>
-			 </select>
-			</section>
+					<option value="day">Day</option>
+					<option value="week">Week</option>
+					<option value="month">Month</option>
+					<option value="year">Year</option>
+				 </select>
+			</fieldset>
+			</br>
+			<fieldset>
+				<legend>Weekdays</legend>
+				<table>
+				<tr>
+					<td><input type="checkbox" id="sunday" name="weekdaySunday" value="true"></td>
+					<td><label for="sunday">Sunday</label></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox" id="monday" name="weekdayMonday" value="true"></td>
+					<td><label for="monday">Monday</label></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox" id="tuesday" name="weekdayTuesday" value="true"></td>
+					<td><label for="tuesday">Tuesday</label></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox" id="wednesday" name="weekdayWednesday" value="true"></td>
+					<td><label for="wednesday">Wednesday</label></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox" id="thursday" name="weekdayThursday" value="true"></td>
+					<td><label for="thursday">Thursday</label></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox" id="friday" name="weekdayFriday" value="true"></td>
+					<td><label for="friday">Friday</label></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox" id="saturday" name="weekdaySaturday" value="true"></td>
+					<td><label for="saturday">Saturday</label></td>
+				</tr>
+				</table>	
+			</fieldset>
+			</br>
+			<fieldset>
+				<legend>Day of Month</legend>
+				<select name="dayOfMonthOrdinal">
+					<option value="first">First</option>
+					<option value="second">Second</option>
+					<option value="third">Third</option>
+					<option value="fourth">Fourth</option>
+					<option value="last">Last</option>
+				</select>
+				<select name="dayOfMonthWeekday">
+					<option value="sunday">Sunday</option>
+					<option value="monday">Monday</option>
+					<option value="tuesday">Tuesday</option>
+					<option value="wednesday">Wednesday</option>
+					<option value="thursday">Thursday</option>
+					<option value="thursday">Friday</option>
+					<option value="thursday">Saturday</option>
+				</select>
+			</fieldset>
+			</br>
+			<fieldset>
+				<legend>Repeating Stops</legend>
+				<select name="repeatStop">
+					<option value="never">Never</option>
+					<option value="number">After # of Repeats</option>
+					<option value="date">After a date</option>
+				</select>
+				<input type="number" id="repeatStopCount" name="repeatStopCount" min="1" max="999" step="1" value="1">
+				<input type="date" id="repeatStopDate" name="repeatStopDate"> 
+			</fieldset>
 
-			Name: <input type="text" name="name"/>
-			<br/>
-			Email: <input type="text" name="email"/>
 		</form>
 		`);
 
