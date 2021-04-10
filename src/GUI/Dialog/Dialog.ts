138db89b049b106f4e1190b08575876823cd696a
BeatLink <beatlink+git@simplelogin.co>
2021-04-10 18:44:22 -0500

    Move GUI form data fetching to other class

--- src/GUI/Dialog/Dialog.ts
+++ src/GUI/Dialog/Dialog.ts
@@ -1,13 +1,6 @@
 import joplin from 'api';
-
-function loadDialogData() {
-
-
-}
-
-function getDialogData() {
-
-}
+import { DialogResult } from 'api/types';
+import { Recurrence } from '../../Logic/recurrence';
 
 export async function createDialog(){
     const Dialog = await joplin.views.dialogs.create('Dialog');
@@ -17,3 +10,36 @@ export async function createDialog(){
     await joplin.views.dialogs.addScript(Dialog, './GUI/Dialog/Dialog.css')
     return Dialog;
 }
+
+async function loadRecurrence(dialogHandle, recurrenceData:Recurrence){
+    
+}
+
+
+async function getRecurrence(recurrenceFormData){
+    var newRecurrenceData = recurrenceFormData.formData.recurrence;    
+    var recurrence = new Recurrence();
+    recurrence.enabled = ('Enabled' in newRecurrenceData ? true : false);    
+    recurrence.intervalNumber = Number(newRecurrenceData.IntervalNumber);
+    recurrence.interval = newRecurrenceData.Interval;
+    recurrence.weekdays.sunday = ('weekdaySunday' in newRecurrenceData ? true : false);
+    recurrence.weekdays.monday = ('weekdayMonday' in newRecurrenceData ? true : false);
+    recurrence.weekdays.tuesday = ('weekdayTuesday' in newRecurrenceData ? true : false);
+    recurrence.weekdays.wednesday = ('weekdayWednesday' in newRecurrenceData ? true : false);
+    recurrence.weekdays.thursday = ('weekdayThursday' in newRecurrenceData ? true : false);
+    recurrence.weekdays.friday = ('weekdayFriday' in newRecurrenceData ? true : false);
+    recurrence.weekdays.saturday = ('weekdaySaturday' in newRecurrenceData ? true : false);
+    recurrence.weekdayOfMonth.ordinal = newRecurrenceData.dayOfMonthOrdinal;
+    recurrence.weekdayOfMonth.weekday = newRecurrenceData.dayOfMonthWeekday;
+    recurrence.stopInfo.type = newRecurrenceData.StopType;
+    recurrence.stopInfo.date = (newRecurrenceData.StopDate ? new Date(newRecurrenceData.StopDate) : null);
+    recurrence.stopInfo.number = Number(newRecurrenceData.StopNumber);
+    return recurrence;
+}
+
+export async function openDialog(dialogHandle, RecurrenceData) {
+    await loadRecurrence(dialogHandle, RecurrenceData);
+    const result = await joplin.views.dialogs.open(dialogHandle);
+    return (result.id == 'ok'? await getRecurrence(result) : null);
+}
+
--- src/Logic/core.ts
+++ src/Logic/core.ts
@@ -1,69 +1,20 @@
-import { DialogResult } from 'api/types';
-import { createDialog } from '../GUI/Dialog/Dialog';
+import { createDialog, openDialog } from '../GUI/Dialog/Dialog';
 import { setupToolbar } from '../GUI/Toolbar/Toolbar';
-import { Recurrence } from './recurrence';
-
-
 //import { setupDatabase } from '../Archive/Database/database';
 
-// GUI ############################################################################################
-async function getResults(result:DialogResult){
-    if (result.id == 'cancel') {
-        console.info('Dialog cancelled');
-        return;
-    }
+var dialog = null;
 
-
-    var recurrenceData = result.formData.recurrence;
-    console.info('Got result: ' + JSON.stringify(recurrenceData));
-    
-    var recurrence = new Recurrence();
-    
-    recurrence.enabled = ('Enabled' in recurrenceData ? true : false);    
-    recurrence.intervalNumber = recurrenceData.IntervalNumber;
-    recurrence.interval = recurrenceData.Interval;
-    recurrence.weekdays.sunday = ('weekdaySunday' in recurrenceData ? true : false);
-    recurrence.weekdays.monday = ('weekdayMonday' in recurrenceData ? true : false);
-    recurrence.weekdays.tuesday = ('weekdayTuesday' in recurrenceData ? true : false);
-    recurrence.weekdays.wednesday = ('weekdayWednesday' in recurrenceData ? true : false);
-    recurrence.weekdays.thursday = ('weekdayThursday' in recurrenceData ? true : false);
-    recurrence.weekdays.friday = ('weekdayFriday' in recurrenceData ? true : false);
-    recurrence.weekdays.saturday = ('weekdaySaturday' in recurrenceData ? true : false);
-    recurrence.weekdayOfMonth.ordinal = recurrenceData.dayOfMonthOrdinal;
-    recurrence.weekdayOfMonth.weekday = recurrenceData.dayOfMonthWeekday;
-    
-    console.debug(recurrence.interval);
-    console.debug(recurrence.weekdays)
-    console.debug(recurrence.enabled);
-    console.debug(recurrence.intervalNumber);
-
-
-
-    console.log(recurrence.enabled)
-
-    /*Got result: 
-    {
-        "id":"ok",
-        "formData": {
-            "recurrence":{
-                "Enabled":"True",
-                "IntervalNumber":"6",
-                "Interval":"week",
-
-                "weekdayMonday":"true",
-                "weekdayThursday":"true",
-                
-                "dayOfMonthOrdinal":"first",
-                "dayOfMonthWeekday":"sunday",
-                "StopDropdown":"number",
-                "StopNumber":"1",
-                "StopDate":""}}}
-    */
+async function openRecurrenceDialog(){
+    //get recurrence data from persistence
+    var recurrenceResult = await openDialog(dialog, null);         // open dialog with recurrence data
+    console.log(recurrenceResult)
+    //save results from dialog to persistence
 
 }
-    
+
 async function setupGUI(){
-    await setupToolbar(await createDialog(), getResults);
+    dialog = await createDialog();
+    await setupToolbar(openRecurrenceDialog);
 }
 
 
@@ -75,7 +26,6 @@ export async function main() {
 
     //Setup UI
     await setupGUI();
-
 }
 
 
