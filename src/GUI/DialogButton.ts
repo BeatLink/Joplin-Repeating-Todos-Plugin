4c1d3c315f0bd7ba6e84bd3380f9a343b2bfae4e
BeatLink <beatlink+git@simplelogin.co>
2021-06-29 19:55:13 -0500

    update symbol names and comments

--- src/GUI/DialogButton.ts
+++ src/GUI/DialogButton.ts
@@ -1,23 +1,21 @@
-// Imports ########################################################################################
+/* Imports ***************************************************************************************/
 import joplin from 'api';
 import {ToolbarButtonLocation } from 'api/types';
-import { openRecurrenceDialog} from './Dialog/Dialog';
+import { onRecurrenceDialogButtonClicked } from '../Logic/core'
 
-// SetupDialotButton ##############################################################################
-/*
+/* setupDialogButton ******************************************************************************
     Sets up the button on the toolbar that opens the recurrence dialog
 */
 export async function setupDialogButton(){
-    await joplin.commands.register({
-        name: 'editRecurrence',
-        label: 'Open Recurrence Dialog',
-        iconName: 'fas fa-redo-alt',
-        execute: async () => {openRecurrenceDialog();},
+    await joplin.commands.register({                            // Register the open recurrence dialog command
+        name: 'openRecurrenceDialog',                           // Set the name of the command
+        label: 'Open Recurrence Dialog',                        // Set the label for the command
+        iconName: 'fas fa-redo-alt',                            // Set the icon for the command
+        execute: async () => {onRecurrenceDialogButtonClicked();},
     });
-    await joplin.views.toolbarButtons.create(
-        'editRecurrenceButton', 
-        'editRecurrence', 
-        ToolbarButtonLocation.NoteToolbar
+    await joplin.views.toolbarButtons.create(                   // Creat the open dialog button and bind it to the command
+        'openRecurrenceDialogButton',                           // The name of the button
+        'openRecurrenceDialog',                                 // The above command to bind the button to
+        ToolbarButtonLocation.NoteToolbar                       // The location of the new button (on the note toolbar)
     );
 }
-
