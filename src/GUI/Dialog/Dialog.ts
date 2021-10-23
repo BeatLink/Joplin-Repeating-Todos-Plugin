52185d02bc2b6816ea47b88f1a9bcc50f409ff17
BeatLink <beatlink+git@simplelogin.co>
2021-10-23 18:03:35 -0500

    Rewrite dialog

--- src/GUI/Dialog/Dialog.ts
+++ src/GUI/Dialog/Dialog.ts
@@ -1,33 +1,81 @@
-// Imports ########################################################################################
+/** Imports ****************************************************************************************************************************************/
 import joplin from 'api';
-import { Recurrence } from '../../Logic/recurrence';
+import { Recurrence} from '../../Logic/recurrence';
+
 
 var dialog = null;
 
+
+/** createDialog ************************************************************************************************************************************
+ * Initializes the recurrence dialog                                                                                                                *
+ ***************************************************************************************************************************************************/
 export async function createDialog(){
     dialog = await joplin.views.dialogs.create('Dialog');
     await joplin.views.dialogs.addScript(dialog, './GUI/Dialog/Dialog.js')
     await joplin.views.dialogs.addScript(dialog, './GUI/Dialog/Dialog.css')
 }
 
+
+/** openDialog **************************************************************************************************************************************
+ * Opens the recurrence dialog for the given noteID                                                                                                 *
+ ***************************************************************************************************************************************************/
 export async function openDialog(recurrenceData){
-    await setRecurrence(recurrenceData)                          // load recurrence into dialog
-    var formResult = await joplin.views.dialogs.open(dialog);             // Show Dialog
-    return await getRecurrence(formResult)
-}
-
-async function setRecurrence(recurrenceData:Recurrence){
     const DialogHTML = await require('./Dialog.html').default;
-    var replacedHTML = DialogHTML.replace("RECURRENCE_DATA", btoa(recurrenceData.toJSON()))
+    var replacedHTML = DialogHTML.replace("RECURRENCE_DATA", btoa(recurrenceToJSON(recurrenceData)))
+    console.log(recurrenceData)
     await joplin.views.dialogs.setHtml(dialog, replacedHTML);
+    var formResult = await joplin.views.dialogs.open(dialog)
+    if (formResult.id == 'ok') {
+        return recurrenceFromJSON(atob(formResult.formData.recurrenceForm.recurrenceData))
+    }
+
 }
 
-async function getRecurrence(formResult){
-    if (formResult.id == 'ok') {
-        var encodedRecurrenceData = formResult.formData.recurrenceForm.recurrenceData                   // gets the encoded recurrence data from the hidden form
-        var decodedRecurrenceData = atob(encodedRecurrenceData)             // decodes the recurrence data into the json string
-        var recurrence = new Recurrence()
-        recurrence.fromJSON(decodedRecurrenceData)
-        return recurrence
-    }
+
+/** recurrenceToJSON ********************************************************************************************************************************
+ * Save recurrence data as a json string                                                                                                            *
+ ***************************************************************************************************************************************************/
+function recurrenceToJSON(recurrence){
+    return JSON.stringify({
+        enabled: recurrence.enabled,
+        interval: recurrence.interval,
+        intervalNumber: recurrence.intervalNumber,
+        weekSunday: recurrence.weekSunday,
+        weekMonday: recurrence.weekMonday,
+        weekTuesday: recurrence.weekTuesday,
+        weekWednesday: recurrence.weekWednesday,
+        weekThursday: recurrence.weekThursday,
+        weekFriday: recurrence.weekFriday,
+        weekSaturday: recurrence.weekSaturday,
+        monthOrdinal: recurrence.monthOrdinal,
+        monthWeekday: recurrence.monthWeekday,
+        stopType: recurrence.stopType,
+        stopDate: recurrence.stopDate,
+        stopNumber: recurrence.stopNumber,
+    })
+}
+
+
+/** recurrenceFromJSON **************************************************************************************************************************
+ * Loads Recurrence data from a JSON string                                                                                                     *
+ ***********************************************************************************************************************************************/
+function recurrenceFromJSON(JSONstring){
+    var parsedJSON = JSON.parse(JSONstring)
+    var recurrence = new Recurrence()
+    recurrence.enabled = Boolean(parsedJSON.enabled)
+    recurrence.intervalNumber = Number(parsedJSON.intervalNumber)
+    recurrence.interval = String(parsedJSON.interval)
+    recurrence.weekSunday = Boolean(parsedJSON.weekSunday)
+    recurrence.weekMonday = Boolean(parsedJSON.weekMonday)
+    recurrence.weekTuesday = Boolean(parsedJSON.weekTuesday)
+    recurrence.weekWednesday = Boolean(parsedJSON.weekWednesday)
+    recurrence.weekThursday = Boolean(parsedJSON.weekThursday)
+    recurrence.weekFriday = Boolean(parsedJSON.weekFriday)
+    recurrence.weekSaturday = Boolean(parsedJSON.weekSaturday)
+    recurrence.monthOrdinal = String(parsedJSON.monthOrdinal)
+    recurrence.monthWeekday = String(parsedJSON.monthWeekday)
+    recurrence.stopType = String(parsedJSON.stopType)
+    recurrence.stopDate = String(parsedJSON.stopDate)
+    recurrence.stopNumber = Number(parsedJSON.stopNumber)
+    return recurrence
 }
