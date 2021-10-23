992434cc8cb3a18d7fc615f3f7a32ba814c761ba
BeatLink <beatlink+git@simplelogin.co>
2021-10-23 18:03:46 -0500

    Update Dialog

--- src/GUI/DialogButton.ts
+++ src/GUI/DialogButton.ts
@@ -1,21 +1,24 @@
-/* Imports ***************************************************************************************/
+/** Imports ****************************************************************************************************************************************/
 import joplin from 'api';
-import {ToolbarButtonLocation } from 'api/types';
-import { onRecurrenceDialogButtonClicked } from '../Logic/core'
+import { ToolbarButtonLocation } from 'api/types';
+import { openRecurrenceDialog } from '../Logic/core';
 
-/* setupDialogButton ******************************************************************************
-    Sets up the button on the toolbar that opens the recurrence dialog
-*/
+/** setupDialogButton *******************************************************************************************************************************
+ * Sets up the button on the toolbar that opens the recurrence dialog                                                                               *
+ ***************************************************************************************************************************************************/
 export async function setupDialogButton(){
-    await joplin.commands.register({                            // Register the open recurrence dialog command
-        name: 'openRecurrenceDialog',                           // Set the name of the command
-        label: 'Open Recurrence Dialog',                        // Set the label for the command
-        iconName: 'fas fa-redo-alt',                            // Set the icon for the command
-        execute: async () => {onRecurrenceDialogButtonClicked();},
+    await joplin.commands.register({
+        name: 'openRecurrenceDialog',
+        label: 'Open Recurrence Dialog',
+        iconName: 'fas fa-redo-alt',
+        execute: async () => {
+            openRecurrenceDialog();
+        },
     });
-    await joplin.views.toolbarButtons.create(                   // Creat the open dialog button and bind it to the command
-        'openRecurrenceDialogButton',                           // The name of the button
-        'openRecurrenceDialog',                                 // The above command to bind the button to
-        ToolbarButtonLocation.NoteToolbar                       // The location of the new button (on the note toolbar)
+    await joplin.views.toolbarButtons.create(
+        'openRecurrenceDialogButton',
+        'openRecurrenceDialog',
+        ToolbarButtonLocation.NoteToolbar
     );
 }
+
